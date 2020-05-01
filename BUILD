9463d68bcc04783ab8de8dbe4eff3fe2b16e45d2
Peter Pedron <peterpedron@gmail.com>
2020-05-01 14:39:25 +0200

    Add first bazel java executable

---
+++ BUILD
@@ -0,0 +1,7 @@
+load("@rules_java//java:defs.bzl", "java_binary")
+
+java_binary(
+    name = "Watten",
+    srcs = glob(["src/main/java/st/watten/*.java"]),
+    main_class = "st.watten.Watten",
+)
---
+++ src/main/java/st/watten/Watten.java
@@ -0,0 +1,9 @@
+package st.watten;
+
+import java.lang.System;
+
+public class Watten {
+    public static void main(String[] args) {
+        System.out.println("This is bazelized Watten!");
+    }
+}
